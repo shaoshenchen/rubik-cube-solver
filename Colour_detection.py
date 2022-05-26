072ed8fcffd075fc392839a28ecf0a303a3f99fc
shaun <45918719+shaoshenchen@users.noreply.github.com>
2022-05-27 00:48:07 +0800

    Add files via upload

---
+++ Colour_detection.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+import Knn
+
+'''
+Detects the Colour of each pixel in runtime. 
+1. Marks the ROI on the cam feed
+2. crops the 9 pixels inside the ROI after converting it into HSV space
+3. Extracts histogram of Hue and Saturation channels
+4. Sends an array of peak histogram values of Hue and Saturation to Knn to detect colours
+'''
+Face_cordintes = np.array([[(180, 110), (240, 165)], [(270, 110), (330, 165)], [(360, 110), (420, 165)],
+                           [(190, 195), (250, 250)], [(275, 195), (335, 250)], [(360, 195), (420, 250)],
+                           [(195, 280), (250, 325)], [(280, 280), (330, 325)], [(360, 280), (410, 325)]])
+
+Faces = np.array(([np.zeros((55, 60, 3)), np.zeros((55, 60, 3)), np.zeros((55, 60, 3)),
+                   np.zeros((55, 60, 3)), np.zeros((55, 60, 3)), np.zeros((55, 60, 3)),
+                   np.zeros((45, 55, 3)), np.zeros((45, 50, 3)), np.zeros((45, 50, 3))]), dtype=object)
+
+cap = cv2.VideoCapture(1)
+
+
+def detect_colour(Faces):
+    feature_matrix = np.zeros((9, 2))
+    for i, face in enumerate(Faces):
+        hue = face[..., 0].flatten()
+        sat = face[..., 1].flatten()
+        hist_hue, hist_sat = np.histogram(hue, bins=np.arange(256)), np.histogram(sat, bins=np.arange(256))
+        feature_matrix[i] = [np.argmax(hist_hue[0]), np.argmax(hist_sat[0])]
+    print(feature_matrix)
+    predicted_colours = Knn.predict_colour(feature_matrix)
+
+
+while (True):
+    _, frame = cap.read()
+    frame_copy = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    i = 0
+    for cordinates in Face_cordintes:
+        point1 = tuple(cordinates[0])
+        point2 = tuple(cordinates[1])
+        cv2.rectangle(frame, point1, point2, (0, 0, 0), 2)
+
+        x_len = abs(cordinates[0, 0] - cordinates[1, 0])
+        y_len = abs(cordinates[0, 1] - cordinates[1, 1])
+
+        Faces[i] = frame_copy[cordinates[0, 1]:cordinates[1, 1], cordinates[0, 0]:cordinates[1, 0]]
+        i += 1
+
+    detect_colour(Faces)
+    cv2.imshow("img", frame)
+
+    if (cv2.waitKey(1) == 27):
+        break
---
+++ Knn.py
@@ -0,0 +1,71 @@
+import numpy as np
+import math
+import cv2
+from scipy import stats
+
+'''This script Loads the Training Dataset and then performs k-Nearest-Neighbouring algorithm on the provided dataset.
+the Colour_detection.py sends the features (max hue and sat from theor respective histograms) of the 9 pixels of the cube
+'''
+
+fh = open("./Dataset/Complete_data.txt", 'r')
+data = np.array([])
+line = fh.readlines()
+data = np.append(data, line)
+np.random.shuffle(data)
+fh.close()
+
+X0 = np.ones(len(data))
+X = np.ones((len(data), 2))
+X1 = np.array([], dtype=int)
+X2 = np.array([])
+Y = np.array([])
+# print(data[0].split(',')[0])
+
+for line in data:
+    X1 = np.append(X1, int(line.split(',')[0]))
+    X2 = np.append(X2, int(line.split(',')[1]))
+    Y = np.append(Y, line.split(',')[2])
+
+X[..., 0] = X1
+X[..., 1] = X2
+
+def hashmap(color):
+  return {
+    'o': '橙色',
+    'w': '白色',
+    'p': '粉色',
+    'y': '黄色',
+    'g': '绿色',
+    'b': '蓝色'
+  }[color]
+
+def predict_colour(feature_Mat):
+    final_prediction_seq = np.empty(9, dtype=str)
+    for i, feature in enumerate(feature_Mat):
+        final_prediction_seq[i] = str (predict_single_face_colour(feature))
+    print(list(map(hashmap, final_prediction_seq)))
+
+def predict_single_face_colour(features):
+    closest_dist = np.zeros((540))
+    closest_label = np.empty(540, dtype=object)
+    for i, training in enumerate(X):
+        Htrain, Strain = training[0], training[1]
+        closest_dist[i] = math.sqrt(math.pow((features[0] - Htrain), 2) + math.pow(features[1] - Strain, 2))
+        closest_label[i] = Y[i]
+    u = np.transpose(np.array([closest_dist, closest_label]))
+    sorted = u[u[:, 0].argsort()]
+
+    colour = stats.mode(sorted[1:20, 1])
+    final_colour = str(colour[0])
+    return final_colour[2]
+
+
+def create_features():
+    img = cv2.imread("60.jpg")
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h, s = img[..., 0].flatten(), img[..., 1].flatten()
+    hist_hue, hist_sat = np.histogram(h, bins=np.arange(256)), np.histogram(s, np.arange(256))
+    hue, sat = np.argmax(hist_hue[0]), np.argmax(hist_sat[0])
+    predict_colour(np.array([hue, sat]))
+
+# create_features()
